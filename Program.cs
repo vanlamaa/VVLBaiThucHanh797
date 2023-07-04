37d428105d505a399dae4c3e587871dbf4a54238
atung8919 <atung8919@gmail.com>
2023-07-04 17:13:40 +0700

    Create_Project

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        console.Write("Nhap vao du lieu: ");
-        string str = console.ReadLine();
-        console.WriteLine("du lieu vua nhap vao la:" + str);
+        Console.Write("Nhap vao du lieu: ");
+        string str = Console.ReadLine();
+        Console.WriteLine("du lieu vua nhap vao la:" + str);
+        string hoTen = "Vu Van Lam";
+        int tuoi = 22;
+        Console.WriteLine("sinh viên:{0} - {1}tuổi", hoTen,tuoi);
     }
 }
