d077af8c71ce962398e2f4eca5f9819bede4d0fa
atung8919 <atung8919@gmail.com>
2023-07-05 14:13:08 +0700

    Create_Project_BaiThucHanh0307

---
+++ BaiThucHanh0307/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhap vao du lieu: ");
+        string str = Console.ReadLine();
+        Console.WriteLine("du lieu vua nhap vao la:" + str);
+    }
+}
